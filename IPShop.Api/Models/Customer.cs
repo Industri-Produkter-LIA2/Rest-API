600d4cb24b059c5f7fb02eebc0afe95ee223faf1
Ahmeedeo <ahmadfaisalhameedi72@gmail.com>
2026-03-27 13:33:05 +0100

    Added Customer API with update functionality

---
+++ IPShop.Api/Models/Customer.cs
@@ -0,0 +1,9 @@
+namespace IPShop.Api.Models;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Company { get; set; }
+    public string Email { get; set; }
+}

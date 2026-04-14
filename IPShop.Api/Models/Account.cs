9c980a0ea73016c3a4f6d541ba4cd9d321e49ec2
Paloftzer <phille04@outlook.com>
2026-04-14 10:29:05 +0200

    Finished backend login logic.

--- IPShop.Api/Models/Account.cs
+++ IPShop.Api/Models/Account.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace IPShop.Api.Models;
 
 public class Account
 {
     public int Id { get; set; }
+
+    [Required]
     public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(6)]
     public string Password { get; set; } = string.Empty;
+
+    [Required]
     public string Email { get; set; } = string.Empty;
+
     public string Role { get; set; } = string.Empty;
 
     public bool IsApproved { get; set; }
